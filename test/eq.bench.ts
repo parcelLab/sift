08c7c14d8fc7852a06e0cc03da439b2e94197915
chiawei <chiawei.ong@parcellab.com>
2024-06-07 18:41:53 +0200

    test: add 10/1000ms benchmark scenarios

--- test/eq.bench.ts
+++ test/eq.bench.ts
@@ -1,75 +1,60 @@
-import { bench, describe } from "vitest";
+import { BenchOptions, bench, describe } from "vitest";
 import { compile } from "../src/compiler";
 import sift from "sift";
 
-describe("simple $eq", () => {
-	const query = { foo: "baz" };
-	const input = [{}, { foo: "bar" }, { foo: "baz" }];
+const benchmarkScenarios: BenchOptions[] = [
+	{ time: 10, warmupTime: 10 },
+	{ time: 1000, warmupTime: 1000 },
+];
 
-	bench("sift", () => {
-		const filter = sift(query);
-		input.filter(filter);
-	});
+const applySift = (query: any, input: any[]) => {
+	const filter = sift(query);
+	input.filter(filter);
+};
 
-	bench("compiled", () => {
-		const filter = compile(query);
-		input.filter(filter);
-	});
-});
+const applyCompiled = (query: any, input: any[]) => {
+	const filter = compile(query);
+	input.filter(filter);
+};
 
-describe("nested path $eq", () => {
-	const query = { "foo.bar": "baz" };
-	const input = [{}, { foo: "bar" }, { foo: "baz" }];
+const benchmarkCases = [
+	{
+		name: "simple $eq",
+		query: { foo: "baz" },
+		input: [{}, { foo: "bar" }, { foo: "baz" }],
+	},
+	{
+		name: "nested path $eq",
+		query: { "foo.bar": "baz" },
+		input: [{}, { foo: "bar" }, { foo: "baz" }],
+	},
+	{
+		name: "full object match",
+		query: { "foo.bar": { baz: "qux" } },
+		input: [
+			{ foo: { bar: { baz: "qux" } } },
+			{ foo: { bar: { baz: "qux", bla: "jaz" } } },
+			{},
+			{ foo: "bar" },
+			{ foo: { bar: "baz" } },
+		],
+	},
+	{
+		name: "null match",
+		query: { "foo.bar": null },
+		input: [
+			{ foo: { bar: null } },
+			{ foo: { bar: "baz" } },
+			{ foo: null },
+			{ foo: "bar" },
+			{},
+		],
+	},
+];
 
-	bench("sift", () => {
-		const filter = sift(query);
-		input.filter(filter);
-	});
-
-	bench("compiled", () => {
-		const filter = compile(query);
-		input.filter(filter);
-	});
-});
-
-describe("full object match", () => {
-	const query = { "foo.bar": { baz: "qux" } };
-	const input = [
-		{ foo: { bar: { baz: "qux" } } },
-		{ foo: { bar: { baz: "qux", bla: "jaz" } } },
-		{},
-		{ foo: "bar" },
-		{ foo: { bar: "baz" } },
-	];
-
-	bench("sift", () => {
-		const filter = sift(query);
-		input.filter(filter);
-	});
-
-	bench("compiled", () => {
-		const filter = compile(query);
-		input.filter(filter);
-	});
-});
-
-describe("null match", () => {
-	const query = { "foo.bar": null };
-	const input = [
-		{ foo: { bar: null } },
-		{ foo: { bar: "baz" } },
-		{ foo: null },
-		{ foo: "bar" },
-		{},
-	];
-
-	bench("sift", () => {
-		const filter = sift(query);
-		input.filter(filter);
-	});
-
-	bench("compiled", () => {
-		const filter = compile(query);
-		input.filter(filter);
+describe.each(benchmarkScenarios)("benchmark $time ms", (options) => {
+	describe.each(benchmarkCases)("$name", ({ query, input }) => {
+		bench("sift", () => applySift(query, input), options);
+		bench("compiled", () => applyCompiled(query, input), options);
 	});
 });
