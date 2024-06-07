49dd87d9f8919676c4499db4f411d4bad3602448
chiawei <chiawei.ong@parcellab.com>
2024-06-07 17:34:01 +0200

    test: more benchmark scenarios

--- test/eq.bench.ts
+++ test/eq.bench.ts
@@ -31,3 +31,45 @@ describe("nested path $eq", () => {
 		input.filter(filter);
 	});
 });
+
+describe("full object match", () => {
+	const query = { "foo.bar": { baz: "qux" } };
+	const input = [
+		{ foo: { bar: { baz: "qux" } } },
+		{ foo: { bar: { baz: "qux", bla: "jaz" } } },
+		{},
+		{ foo: "bar" },
+		{ foo: { bar: "baz" } },
+	];
+
+	bench("sift", () => {
+		const filter = sift(query);
+		input.filter(filter);
+	});
+
+	bench("compiled", () => {
+		const filter = compile(query);
+		input.filter(filter);
+	});
+});
+
+describe("null match", () => {
+	const query = { "foo.bar": null };
+	const input = [
+		{ foo: { bar: null } },
+		{ foo: { bar: "baz" } },
+		{ foo: null },
+		{ foo: "bar" },
+		{},
+	];
+
+	bench("sift", () => {
+		const filter = sift(query);
+		input.filter(filter);
+	});
+
+	bench("compiled", () => {
+		const filter = compile(query);
+		input.filter(filter);
+	});
+});
